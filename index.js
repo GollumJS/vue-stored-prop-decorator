10fb704276f2fda5339004c0d37a4b6ee0281b2a
SmeagolWorms4 <smeagolworms4@gmail.com>
2020-09-29 21:01:57 +0200

    recursive commit

--- index.js
+++ index.js
@@ -8,15 +8,55 @@ exports.Stored = function (store, propertyName) {
         if (!propertyName) {
             propertyName = propertyKey;
         }
+        var name = 'set' + propertyName.replace(/\b\w/g, function (l) { return l.toUpperCase(); });
         Object.defineProperty(target, propertyKey, {
             get: function () {
+                var _this = this;
+                var origin = null;
                 if (typeof store === 'string') {
-                    return this.$store.state[store][propertyName];
+                    origin = this.$store.state[store][propertyName];
                 }
-                return store().state[propertyName];
+                else {
+                    origin = store().state[propertyName];
+                }
+                if (origin instanceof Object) {
+                    var copy_1 = null;
+                    var createProxy_1 = function (obj) {
+                        if (obj instanceof Object) {
+                            return new Proxy(obj, {
+                                get: function (obj, prop) {
+                                    return createProxy_1(obj[prop]);
+                                },
+                                set: function (obj, prop, value) {
+                                    obj[prop] = value;
+                                    if (typeof store === 'string') {
+                                        _this.$store.commit(store + '/' + name, copy_1);
+                                    }
+                                    else {
+                                        store().commit(name, copy_1);
+                                    }
+                                    return true;
+                                }
+                            });
+                        }
+                        return obj;
+                    };
+                    if (Array.isArray(origin)) {
+                        copy_1 = [];
+                        for (var i = 0; i < origin.length; i++) {
+                            copy_1[i] = origin[i];
+                        }
+                    }
+                    else if (typeof origin.clone === 'function') {
+                        copy_1 = origin.clone();
+                    }
+                    if (copy_1) {
+                        return createProxy_1(copy_1);
+                    }
+                }
+                return origin;
             },
             set: function (value) {
-                var name = 'set' + propertyName.replace(/\b\w/g, function (l) { return l.toUpperCase(); });
                 if (typeof store === 'string') {
                     this.$store.commit(store + '/' + name, value);
                     return;
--- index.ts
+++ index.ts
@@ -6,17 +6,57 @@ export const Stored = function (store: (() => Store<any>)|string, propertyName: 
 		if (!propertyName) {
 			propertyName = propertyKey;
 		}
+        const name = 'set' + propertyName.replace(/\b\w/g, l => l.toUpperCase());
 		
 		Object.defineProperty(target, propertyKey, {
 			get: function () {
+				let origin: any = null;
 				if (typeof store === 'string') {
-					return this.$store.state[(<string>store)][propertyName];
+					origin = this.$store.state[(<string>store)][propertyName];
+				} else {
+					origin = (<() => Store<any>> store)().state[propertyName];
 				}
-				return (<() => Store<any>> store)().state[propertyName];
+				
+                if (origin instanceof Object) {
+
+                    let copy: any = null;
+
+                    const createProxy = (obj) => {
+                        if (obj instanceof Object) {
+                            return new Proxy(obj, {
+                                get: (obj: any, prop: any) => {
+                                    return createProxy(obj[prop]);
+                                },
+                                set: (obj: any, prop: any, value: any): boolean => {
+                                    obj[prop] = value;
+                                    if (typeof store === 'string') {
+										this.$store.commit((<string>store) + '/' + name, copy);
+									} else {
+										(<() => Store<any>> store)().commit(name, copy);	
+									}
+                                    return true;
+                                }
+                            });
+                        }
+                        return obj;
+                    };
+
+                    if (Array.isArray(origin)) {
+                        copy = [];
+                        for (let i = 0; i < origin.length; i++) {
+                        	copy[i] = origin[i];
+                        }
+                    } else if (typeof origin.clone === 'function') {
+                        copy = origin.clone();
+                    }
+
+                    if (copy) {
+                        return createProxy(copy);
+                    }
+                }
+                return origin;
 			},
 			set: function (value: any) {
-				const name = 'set' + propertyName.replace(/\b\w/g, l => l.toUpperCase());
-				
 				if (typeof store === 'string') {
 					this.$store.commit((<string>store) + '/' + name, value);
 					return;
